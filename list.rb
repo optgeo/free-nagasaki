687a3b6d980c58aed9c0032953faaee0ada144a6
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2023-03-15 05:30:44 +0900

    update

--- list.rb
+++ list.rb
@@ -1,9 +1,16 @@
-File.open('list.md', 'w') {|w|
-  File.foreach('convert/dst/list.csv') {|l|
-    r = l.strip.split(',')
-    w.print <<-EOS
-1. [#{r[0]} ipfs:#{r[1]}](https://viewer.copc.io/?copc=https://smb.optgeo.org/ipfs/#{r[1]})
-    EOS
-  }
+list = File.open('list.md', 'w')
+pin = File.open('pin.sh', 'w')
+
+File.foreach('convert/dst/list.csv') {|l|
+  r = l.strip.split(',')
+  (fn, cid) = r
+  list.print <<-EOS
+1. [#{fn} ipfs:#{cid}](https://viewer.copc.io/?copc=https://smb.optgeo.org/ipfs/#{cid})
+  EOS
+  pin.print <<-EOS
+ipfs pin add --progress #{cid}
+  EOS
 }
 
+list.close
+pin.close
