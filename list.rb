43723193af8cc4cc450319b6beeee9bc0e055616
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2023-03-15 00:12:03 +0900

    update

--- list.rb
+++ list.rb
@@ -2,7 +2,7 @@ File.open('list.md', 'w') {|w|
   File.foreach('convert/dst/list.csv') {|l|
     r = l.strip.split(',')
     w.print <<-EOS
-1. [#{r[0]}](https://viewer.copc.io/?copc=https://smb.optgeo.org/ipfs/#{r[1]})
+1. [#{r[0]} ipfs:#{r[1]}](https://viewer.copc.io/?copc=https://smb.optgeo.org/ipfs/#{r[1]})
     EOS
   }
 }
